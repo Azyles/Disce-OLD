fa7d5094bb58dce608a4640d11ab962a0a0fe04d
KingRegera <56901151+KingRegera@users.noreply.github.com>
2020-05-29 11:52:13 -0700

    first commit

---
+++ Disce/AuthView.swift
@@ -0,0 +1,21 @@
+//
+//  AuthView.swift
+//  Disce
+//
+//  Created by Kushagra Singh on 5/29/20.
+//  Copyright © 2020 PolyNexus. All rights reserved.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
---
+++ Disce/SessionStore.swift
@@ -0,0 +1,9 @@
+//
+//  SessionStore.swift
+//  Disce
+//
+//  Created by Kushagra Singh on 5/29/20.
+//  Copyright © 2020 PolyNexus. All rights reserved.
+//
+
+import Foundation
